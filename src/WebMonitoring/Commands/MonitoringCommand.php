35859256fffe440a208be79c64e60e3669c4c817
Jakub Englický <jakubenglicky@gmail.com>
2017-08-29 22:09:12 +0200

    add Monitors class

--- src/WebMonitoring/Commands/MonitoringCommand.php
+++ src/WebMonitoring/Commands/MonitoringCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace jakubenglicky\WebMonitoring\Commands;
 
+use jakubenglicky\WebMonitoring\Controls\Monitors;
 use Nette\Neon\Neon;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -15,13 +16,15 @@ class MonitoringCommand extends Command
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$file = file_get_contents(__DIR__ . '/../../../monitors/pavel_vana_cz.neon');
-		$data = Neon::decode($file);
+		$monitors = new Monitors();
 
-		foreach($data as $name => $values) {
+		$data = $monitors->getMonitors();
+
+		foreach ($data as $name => $values) {
 			$output->writeln($name);
-			$output->writeln($values['url']);
+			$output->writeln(print_r($values));
 		}
+
 	}
 
 }
--- src/WebMonitoring/Controls/Monitors.php
+++ src/WebMonitoring/Controls/Monitors.php
@@ -3,7 +3,33 @@
 namespace jakubenglicky\WebMonitoring\Controls;
 
 
+use Nette\Neon\Neon;
+
 class Monitors
 {
+	const folder = __DIR__ . '/../../../monitors/';
+
+	public function mergeMonitorsFiles()
+	{
+		$files = scandir(self::folder);
+
+		$result = NULL;
+		foreach($files as $file) {
+
+			if (!in_array($file,array(".","..")))
+			{
+				$result .= file_get_contents(self::folder . $file);
+				$result .= PHP_EOL;
+			}
+		}
+
+		return $result;
+	}
+
+	public function getMonitors()
+	{
+		return Neon::decode($this->mergeMonitorsFiles());
+	}
+
 
 }
